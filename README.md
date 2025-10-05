ce71fa0ad7d72b7138184dcf17e0508c6dc61412
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-10-05 13:57:55 +0300

    added: README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+This folder contains my solution for holberton web backend projects.
---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Math functions"""
+
+
+def add(a: float, b: float) -> float:
+    """add a and b"""
+    return a + b
