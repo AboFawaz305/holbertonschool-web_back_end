0a8c0eac60e524507bebce1ea1f7b40822905841
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-10-06 17:34:34 +0300

    added: project 23 task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
-"""
-Async/Await
-"""
+"""Async/Await"""
 import asyncio
 import typing
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_append(res: typing.List[float], max_delay: int):
-    """Wait and append the result"""
-    res.append(await wait_random(max_delay))
-
-
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """spawn wait_random n times and wait for its value"""
     res = []
 
-    async with asyncio.TaskGroup() as tg:
-        for _ in range(n):
-            tg.create_task(wait_append(res, max_delay))
-        return res
+    async def wait_append():
+        """Wait and append the result"""
+        nonlocal res
+        res.append(await wait_random(max_delay))
+
+    await asyncio.gather(*[wait_append() for _ in range(n)])
+    return res
 
 
 if __name__ == "__main__":
