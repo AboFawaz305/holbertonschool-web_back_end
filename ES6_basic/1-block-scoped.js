e100b24fcae6447e03c8c8c78d50d1b97b983fcb
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-10-26 14:24:30 +0300

    add project 27 task 1

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    task && task2;
+  }
+
+  return [task, task2];
+}
