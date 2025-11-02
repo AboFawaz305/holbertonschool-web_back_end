6f34c09bdf6e4e7e6af267521e492839fb2ceec6
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-11-02 17:09:56 +0300

    add project 29 task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw new Error("Position outside range");
+  const arr = new ArrayBuffer(length);
+  const view = new DataView(arr);
+  view.setInt8(position, value);
+  return view;
+}
