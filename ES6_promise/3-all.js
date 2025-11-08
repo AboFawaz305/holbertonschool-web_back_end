96fa9cfe68c57129f33524b742bfc7f8a8637108
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-11-08 18:17:21 +0300

    add project 30 task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from "./utils.js";
+export default function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then(([UserValues, Photo]) => {
+      console.log(
+        `${Photo.body} ${UserValues.firstName} ${UserValues.lastName}`,
+      );
+    })
+    .catch((error) => {
+      console.log("Signup system offline");
+    });
+}
