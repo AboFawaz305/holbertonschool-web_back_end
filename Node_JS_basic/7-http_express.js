8c6e19591b270622794c9ad6b21b50877fa79e9a
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-11-12 15:21:39 +0300

    add project 31 task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const { readFileSync } = require("node:fs");
+const express = require('express');
+const { readFileSync } = require('node:fs');
 
 const _atry = async (fn, ...args) => {
   try {
@@ -14,57 +14,56 @@ const agaurd = async (msg, fn, ...args) => {
   return result.value;
 };
 async function countStudentsStr(path) {
-  const db = await agaurd("Cannot load the database", readFileSync, path, {
-    encoding: "utf8"
+  const db = await agaurd('Cannot load the database', readFileSync, path, {
+    encoding: 'utf8',
   });
   const [headers, ...rows] = db
-    .split("\n")
-    .map(e => e.split(","))
-    .filter(e => e.length > 1);
-  const fieldIndex = headers.indexOf("field");
-  const firstNameIndex = headers.indexOf("firstname");
+    .split('\n')
+    .map((e) => e.split(','))
+    .filter((e) => e.length > 1);
+  const fieldIndex = headers.indexOf('field');
+  const firstNameIndex = headers.indexOf('firstname');
   const fields = rows.reduce(
     (fl, s) => (fl.includes(s[fieldIndex]) ? fl : fl.concat([s[fieldIndex]])),
-    []
+    [],
   );
-  const firstNamesByField = fields.map(f => [
+  const firstNamesByField = fields.map((f) => [
     f,
-    rows.filter(s => s[fieldIndex] === f).map(s => s[firstNameIndex])
+    rows.filter((s) => s[fieldIndex] === f).map((s) => s[firstNameIndex]),
   ]);
 
   return firstNamesByField
     .reduce(
-      (res, [field, firstNames]) =>
-        res.concat([
-          `Number of students in ${field}: ${
-            firstNames.length
-          }. List: ${firstNames.join(", ")}`
-        ]),
-      [`Number of students: ${rows.length}`]
+      (res, [field, firstNames]) => res.concat([
+        `Number of students in ${field}: ${
+          firstNames.length
+        }. List: ${firstNames.join(', ')}`,
+      ]),
+      [`Number of students: ${rows.length}`],
     )
-    .join("\n");
+    .join('\n');
 }
 
 const app = express();
 module.exports = app;
 
-app.get("/", (_, res) => {
+app.get('/', (_, res) => {
   res.writeHead(200, {
-    "Content-Type": "text/plain"
+    'Content-Type': 'text/plain',
   });
-  res.end("Hello Holberton School!");
+  res.end('Hello Holberton School!');
 });
 
-app.get("/students", async (_, res) => {
+app.get('/students', async (_, res) => {
   res.writeHead(200, {
-    "Content-Type": "text/plain"
+    'Content-Type': 'text/plain',
   });
-  res.write("This is the list of our students\n");
+  res.write('This is the list of our students\n');
   try {
     const stds = await countStudentsStr(process.argv[2]);
     res.end(stds);
   } catch (e) {
-    res.end("Cannot load the database");
+    res.end('Cannot load the database');
   }
 });
 
