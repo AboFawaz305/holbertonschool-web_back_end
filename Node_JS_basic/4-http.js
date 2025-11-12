2b26d5b1f40c3268c974ed62c0f188d02bed86eb
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-11-12 12:23:46 +0300

    add project 31 task 5

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,9 +2,10 @@ const http = require('node:http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, {
-    'content-type': 'text/plain; charset=utf8',
+    'Content-Type': 'text/plain',
   });
   res.end('Hello Holberton School!');
 });
 
+module.exports = app;
 app.listen(1245);
